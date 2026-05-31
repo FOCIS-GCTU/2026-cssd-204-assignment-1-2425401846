467e3e533dc760712adbbfcac48621b63a4fa201
Dziwornu Justice Selasi <2425401846@live.gctu.edu.gh>
2026-05-31 19:40:07 -0300

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,20 @@
-
 def main():
-  pass #code goes here
+#first blank line followed by 3periods and 3 initials and another blank line
 
-
-
+    print()
+    print("...JSD")
+    print()
+#the output of 10 characters high and 60 total character wide
+    print("...JJJJJJJJJJJJ...SSSSSSSSSSSS...DDDDDDDDDDDD...............")
+    print("...JJJJJJJJJJJJ...SSSSSSSSSSSS...DDDDDDDDDDDD...............")
+    print("........JJ.........SSSS..........DDD.......DDD..............")
+    print("........JJ.........SSSS..........DDD........DD..............")
+    print("........JJ..........SSSSSS.......DDD........DD..............")
+    print("........JJ............SSSSSS.....DDD........DD..............")
+    print("...J....JJ................SSSS...DDD........DD..............")
+    print("...JJ...JJ................SSSS...DDD.......DDD..............")
+    print("...JJ...JJ........SSSSSSSSSSSSS...DDDDDDDDDD................")
+    print("...JJJJJJJ.......SSSSSSSSSSSSS....DDDDDDDDD.................")
+#3rd blank line after the output, or initial block arrang..
+    print()
 main()
